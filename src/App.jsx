b545a75e7e5574b894d3726710f42c99a1be3591
di0rio <caua.diorio29@gmail.com>
2023-12-10 17:43:01 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const DraggableItem = ({ item }) => {
+    const [{ isDragging }, drag] = useDrag(() => ({
+      type: "item",
+      item: { id: item.id },
+      collect: (monitor) => ({
+        isDragging: !!monitor.isDragging(),
+      }),
+    }));
 
+    return (
+      <div ref={drag} className="items">
+        {item.text}
+      </div>
+    );
+  };
+
+  const DropZone = ({ title, items, onMoveItem }) => {
+    const [{ isOver }, drop] = useDrop({
+      accept: "item",
+      drop: (draggedItem) => onMoveItem(draggedItem.id),
+      collect: (monitor) => ({
+        isOver: monitor.isOver(),
+      }),
+    });
+
+    return (
+      <div ref={drop} className={`drop-zone ${isOver ? "onDrag" : ""} `}>
+        <h4>{title}</h4>
+        {items.map((item) => (
+          <DraggableItem key={item.id} item={item} />
+        ))}
+      </div>
+    );
+  };
+
+  const [listItem, setListItem] = useState([
+    { id: " 1", text: "Item 1" },
+    { id: " 2", text: "Item 2" },
+    { id: " 3", text: "Item 3" },
+  ]);
+  const [list2Item, setList2Item] = useState([]);
+
+  const handleMoveItem = (itemId, originList, destinationList) => {
+    const item = destinationList.find((item) => item.id === itemId);
+    if (!item) return;
+
+    const newDestinationList = destinationList.filter(
+      (item) => item.id !== itemId
+    );
+    const newOriginList = [...originList, item];
+
+    if (originList === listItem) {
+      setListItem(newOriginList);
+      setList2Item(newDestinationList);
+    } else {
+      setListItem(newDestinationList);
+      setList2Item(newOriginList);
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      <DndProvider backend={HTML5Backend}>
+        <div className="dropContainer">
+          <DropZone
+            title="list 1"
+            items={listItem}
+            onMoveItem={(itemId) => handleMoveItem(itemId, listItem, list2Item)}
+          />
 
-export default App
+          <DropZone
+            title="list 2"
+            items={list2Item}
+            onMoveItem={(itemId) => handleMoveItem(itemId, list2Item, listItem)}
+          />
+        </div>
+      </DndProvider>
+    </div>
+  );
+};
+
+export default App;
